34a6a604a0fca2b5c9f986bec763cfaf794e33fb
Nicole Costa <nicole.r.costa@aluno.senai.br>
2026-03-13 08:54:30 -0300

    Iniciando a lógica

--- comparar-escolas.js
+++ comparar-escolas.js
@@ -1,6 +1,15 @@
 
+const codigoA = document.querySelectorAll('unidade__input unidadeA__codigo');
+const cidadeA = document.querySelectorAll('unidade__input unidadeA__cidade');
+const anoConstrucaoA = document.querySelectorAll('unidade__input unidadeA__ano');
+const qtdeCursosA = document.querySelectorAll('unidade__input unidadeA__cursos');
 
-/* 🚨NÃO ALTERAR A CLASSE 🚨 */
+const codigoB = document.querySelectorAll('unidade__input unidadeB__codigo');
+const cidadeB = document.querySelectorAll('unidade__input unidadeB__cidade');
+const anoConstrucaoB = document.querySelectorAll('unidade__input unidadeB__ano');
+const qtdeCursosB = document.querySelectorAll('unidade__input unidadeB__cursos');
+
+
 
 class Senai {
   constructor(codigo, cidade, anoConstrucao, qtdeCursos) {
@@ -25,29 +34,9 @@ class Senai {
   }
 
 }
-/* 🚨NÃO ALTERAR A CLASSE 🚨 */
 
 
 
-/* SUGESTÃO DE LÓGICA PARA O DESENVOLVIMENTO */
-
-
-
-/* ===== OBTER ELEMENTOS ===== */
-
-const codigoA = document.querySelectorAll('unidade__input unidadeA__codigo');
-const cidadeA = document.querySelectorAll('unidade__input unidadeA__cidade');
-const anoConstrucaoA = document.querySelectorAll('unidade__input unidadeA__ano');
-const qtdeCursosA = document.querySelectorAll('unidade__input unidadeA__cursos');
-
-const codigoB = document.querySelectorAll('unidade__input unidadeB__codigo');
-const cidadeB = document.querySelectorAll('unidade__input unidadeB__cidade');
-const anoConstrucaoB = document.querySelectorAll('unidade__input unidadeB__ano');
-const qtdeCursosB = document.querySelectorAll('unidade__input unidadeB__cursos');
-
-
-
-/* ===== FUNÇÃO DE VALIDAÇÃO ===== */
 
 function mostrarResultado(mensagem) {
   textoResultado.textContent = mensagem;
@@ -60,19 +49,46 @@ if (isNaN(this.codigo) || isNaN(this.cidade) || this.anoConstrucao <= 0 || this.
 
 
 
-/* ===== INSTANCIAR ESCOLA A ===== */
 
 const escolaA = new Senai(codigoA, cidadeA, anoConstrucaoA, qtdeCursosA)
 function instanciarEscolaA(){
-
+  escolaA.querySelectorAll('unidade__botao unidadeA__instanciar')
 }
+
+
+const escolaB = new Senai(codigoB, cidadeB, anoConstrucaoB, qtdeCursosB)
+function instanciarEscolaB(){
+  escolaB.querySelectorAll('unidade__botao unidadeB__instanciar')
+}
+
+
+/* 🚨NÃO ALTERAR A CLASSE 🚨 */
+
+
+/* 🚨NÃO ALTERAR A CLASSE 🚨 */
+
+
+
+/* SUGESTÃO DE LÓGICA PARA O DESENVOLVIMENTO */
+
+
+
+/* ===== OBTER ELEMENTOS ===== */
+
+
+
+/* ===== FUNÇÃO DE VALIDAÇÃO ===== */
+
+
+
+/* ===== INSTANCIAR ESCOLA A ===== */
+
 
 
 /* ===== INSTANCIAR ESCOLA B ===== */
 
 
 /* ===== ABRIR ESCOLA ===== */
-
 
 
 /* ===== FECHAR ESCOLA ===== */
